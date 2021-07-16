3877d0c8426a951dfdc3b4835378b4d42356fefe
Farhan K <farhan.crazyeyes@gmail.com>
2021-07-16 12:51:00 +0000

    Made it into an extension

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-// text box array (This is a queue)
-var textBoxes = [];	
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    function: contentScriptFunc
+  });
+});
 
-// NEED to be expanded to get all types of text boxes.
-function getTextBoxesInPage() {
-	
-	var x = document.getElementsByTagName("input");
+
+function contentScriptFunc() {
+
+	var textBoxes = [];
+  var x = document.getElementsByTagName("input");
 	
 	for (var i = 0; i < x.length; i++) {
 		// 1st->2nd->3rd
 		var y = document.getElementsByTagName("input").item(i);
 		textBoxes.push(y);
 	}
-
-	// for (var j=0; j < textBoxes.length; j++) {
-	// 	textBoxes[j].item(j).focus();
-	// }
 		
+	textBoxes[0].focus();
+		
+	// Remove from beginnig of array
+	var x = textBoxes.shift();
+		
+	// Add this to the end
+	textBoxes.push(x); 
 }
-
-function pressShortcutKey(e) {
-	
-	var e = e || window.event;
-	
-	// After pressing shortcut key, highlight the first textbox in the queue
-	if (e.ctrlKey && e.which == 32) {
-		
-		textBoxes[0].focus();
-		
-		// Remove from beginnig of array
-		var x = textBoxes.shift();
-		
-		// Add this to the end
-		textBoxes.push(x);
-	}
-}
-document.addEventListener('keyup', pressShortcutKey, false);
