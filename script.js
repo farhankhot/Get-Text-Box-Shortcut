3a1ec08c1f13f5ff6803f8bed4d5bfaaeb67772f
Farhan K <farhan.crazyeyes@gmail.com>
2021-07-21 17:23:15 +0000

    Works! Separated extension to content script and background script.

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 // This code: Chrome dev site, https://github.com/shahednasser/chrome-commands-tutorial/blob/master/MV3/background.js
 // Helpful Manifest in the repo as well
 
-
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-// 	if (changeInfo.status === 'completed' && tab.active){
-// 		chrome.scripting.executeScript({
-//     	target: {tabId: tab.id},
-//     	function: contentScriptFunc
-//   	});
-// 	}
-  
-	
-// });
-
 window.onload = function() {
 	var textBoxes = [];
 	// https://stackoverflow.com/questions/16000055/select-all-textboxes-in-a-document-using-javascript
@@ -37,26 +25,3 @@ window.onload = function() {
 	// var ans = sessionStorage.getItem('list');
 	// console.log(JSON.parse(ans));    
 }
-
-// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-// 	if (changeInfo == 'completed' && tab.active){
-// 		var textBoxes = [];
-
-// 		var inputTags = document.getElementsByTagName("input");
-		
-// 		for (var j = 0; j < inputTags.length; j++) {
-			
-// 			if (inputTags[j].getAttribute("type") === "text"
-// 			|| inputTags[j].getAttribute("type") === "password") {
-
-// 				var y = inputTags.item(j).getAttribute("name");
-// 				textBoxes.push(y);
-// 			}
-// 			localStorage.setItem('list',JSON.stringify(textBoxes));
-			
-// 		}
-//   	});
-			
-		
-// 	}
-// });
